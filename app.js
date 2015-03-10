423751451cee6e1f52f24afcf2bb176a8c9898a7
Sophia Haines <sophia.haines@gmail.com>
2015-03-10 12:09:08 +0800

    redoing the radio buttons and prior commit work with cleaner angular

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-(function(){
-  var app = angular.module('talklyceum', []);
+// $('input:radio').click(function(){
+//     $('label#' + $(this).attr('id')).toggleClass('checkedClass'); // checkedClass is defined in your CSS
+// });
 
-})();
+// (function(){
+//   var app = angular.module('talklyceum', []);
+
+
+//   }]);
+// })();
+
+
+angular.module("talk", [])
+  .controller("FormController", function($scope) {
+    $scope.myForm = {};
+
+    $scope.myForm.options = [
+      { id : "bpo", name: "Buisness Process Outsourcing" }
+     ,{ id : "hrm", name: "Hotel & Restaurant Management" }
+     ,{ id : "other", name: "Other" }
+     ];
+  });
